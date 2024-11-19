7758bf3d1112d694a511ba9edeacec3fc4d61ca3
Santiagoroseroq <sroseroq@unal.edu.co>
2024-11-19 10:57:09 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,13 +1,25 @@
 package taller3.televisores;
 
-public class Control {
-    private TV tv;
 
-    public void enlazar(TV tv) {
-        this.tv = tv;
-        tv.setCanal(this);
+public class Control {
+    private TV tv; // Atributo para enlazar el control con un televisor
+
+    // Métodos get y set para el atributo tv
+    public TV getTv() {
+        return tv;
     }
 
+    public void setTv(TV tv) {
+        this.tv = tv;
+    }
+
+    // Método para enlazar el control con un televisor
+    public void enlazar(TV televisor) {
+        this.tv = televisor;          // Asignar el televisor al control
+        televisor.setControl(this);   // Asignar este control al televisor
+    }
+
+    // Métodos para controlar el televisor de manera remota
     public void turnOn() {
         if (tv != null) {
             tv.turnOn();
@@ -22,35 +34,37 @@ public class Control {
 
     public void canalUp() {
         if (tv != null) {
-            tv.setCanal(tv.getCanal() + 1);
+            tv.canalUp();
         }
     }
 
     public void canalDown() {
         if (tv != null) {
-            tv.setCanal(tv.getCanal() - 1);
+            tv.canalDown();
         }
     }
 
     public void volumenUp() {
         if (tv != null) {
-            tv.setVolumen(tv.getVolumen() + 1);
+            tv.volumenUp();
         }
     }
 
     public void volumenDown() {
         if (tv != null) {
-            tv.setVolumen(tv.getVolumen() - 1);
+            tv.volumenDown();
         }
     }
 
-    public void setCanal(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setCanal'");
+    public void setCanal(int canal) {
+        if (tv != null) {
+            tv.setCanal(canal);
+        }
     }
 
-    public void setCanal(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setCanal'");
+    public void setVolumen(int volumen) {
+        if (tv != null) {
+            tv.setVolumen(volumen);
+        }
     }
 }

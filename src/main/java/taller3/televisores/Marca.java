106d3493716dadc592fb6dfb40a5298a8ec4ce85
Santiagoroseroq <sroseroq@unal.edu.co>
2024-11-19 10:57:15 -0500

    Update Marca.java

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -1,16 +1,20 @@
 package taller3.televisores;
 
 public class Marca {
+    // Atributo de la clase
     private String nombre;
 
+    // Constructor que recibe el nombre
     public Marca(String nombre) {
         this.nombre = nombre;
     }
 
+    // Método getter para obtener el nombre
     public String getNombre() {
         return nombre;
     }
 
+    // Método setter para cambiar el nombre
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }

142b24b8bf68d85027c4a18bfa11dfedecd74994
Santiagoroseroq <sroseroq@unal.edu.co>
2024-11-19 13:29:26 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,70 +1,40 @@
 package taller3.televisores;
 
-
 public class Control {
-    private TV tv; // Atributo para enlazar el control con un televisor
+	private TV tv;
+	public void turnOn() {
+		tv.turnOn();
+	}
+	public void turnOff() {
+		tv.turnOff();
+	}
+	public void canalUp() {
+		tv.canalUp();
+	}
+	public void canalDown() {
+		tv.canalDown();
+	}
+	public void volumenUp() {
+		tv.volumenUp();
+	}
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	public void setCanal(int i) {
+		tv.setCanal(i);
+	}
+	public void setVolumen(int j) {
+		tv.setVolumen(j);
+	}
+	public void enlazar(TV televisor) {
+		tv = televisor;
+		televisor.setControl(this);
+	}
+	public void setTv(TV televisor) {
+		this.enlazar(televisor);
+	}
+	public TV getTv() {
+		return tv;
+	}
 
-    // Métodos get y set para el atributo tv
-    public TV getTv() {
-        return tv;
-    }
-
-    public void setTv(TV tv) {
-        this.tv = tv;
-    }
-
-    // Método para enlazar el control con un televisor
-    public void enlazar(TV televisor) {
-        this.tv = televisor;          // Asignar el televisor al control
-        televisor.setControl(this);   // Asignar este control al televisor
-    }
-
-    // Métodos para controlar el televisor de manera remota
-    public void turnOn() {
-        if (tv != null) {
-            tv.turnOn();
-        }
-    }
-
-    public void turnOff() {
-        if (tv != null) {
-            tv.turnOff();
-        }
-    }
-
-    public void canalUp() {
-        if (tv != null) {
-            tv.canalUp();
-        }
-    }
-
-    public void canalDown() {
-        if (tv != null) {
-            tv.canalDown();
-        }
-    }
-
-    public void volumenUp() {
-        if (tv != null) {
-            tv.volumenUp();
-        }
-    }
-
-    public void volumenDown() {
-        if (tv != null) {
-            tv.volumenDown();
-        }
-    }
-
-    public void setCanal(int canal) {
-        if (tv != null) {
-            tv.setCanal(canal);
-        }
-    }
-
-    public void setVolumen(int volumen) {
-        if (tv != null) {
-            tv.setVolumen(volumen);
-        }
-    }
 }

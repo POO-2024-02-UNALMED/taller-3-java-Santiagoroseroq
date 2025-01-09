ad7d5fc98d066cd4bce7d3f3e6066b74101a3285
Santiagoroseroq <sroseroq@unal.edu.co>
2025-01-09 18:39:29 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,40 +1,53 @@
 package taller3.televisores;
 
 public class Control {
-	private TV tv;
-	public void turnOn() {
-		tv.turnOn();
-	}
-	public void turnOff() {
-		tv.turnOff();
-	}
-	public void canalUp() {
-		tv.canalUp();
-	}
-	public void canalDown() {
-		tv.canalDown();
-	}
-	public void volumenUp() {
-		tv.volumenUp();
-	}
-	public void volumenDown() {
-		tv.volumenDown();
-	}
-	public void setCanal(int i) {
-		tv.setCanal(i);
-	}
-	public void setVolumen(int j) {
-		tv.setVolumen(j);
-	}
-	public void enlazar(TV televisor) {
-		tv = televisor;
-		televisor.setControl(this);
-	}
-	public void setTv(TV televisor) {
-		this.enlazar(televisor);
+    private TV tv;
+	public Control() {}
+
+    public Control(TV tv) {
+        this.tv = tv;
+    }
+
+    public void turnOn() {
+        this.tv.turnOn();
+    }
+
+    public void turnOff() {
+        this.tv.turnOff();
+    }
+
+    public void canalUp() {
+        this.tv.canalUp();
+    }
+
+    public void canalDown() {
+        this.tv.canalDown();
+    }
+
+    public void volumenUp() {
+        this.tv.volumenUp();
+    }
+
+    public void volumenDown() {
+        this.tv.volumenDown();
+    }
+
+    public void setCanal(int canal) {
+        this.tv.setCanal(canal);
+    }
+
+    public void setVolumen(int volumen) {
+        this.tv.setVolumen(volumen);
+    }
+	public void enlazar(TV tv) {
+		this.tv = tv;
+		tv.setControl(this);
 	}
 	public TV getTv() {
-		return tv;
-	}
+        return tv;
+    }
 
+    public void setTv(TV tv) {
+        this.tv = tv;
+    }
 }
